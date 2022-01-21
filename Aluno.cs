332ffe5464a28cd7f72184f42d5361110999868b
JonathasLeite <jonathas.leite@yahoo.com.br>
2022-01-21 07:36:03 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioAlexGreen_Udemy004
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+       public void SomaNota()
+        {
+            double NotaFim = Nota1 + Nota2 + Nota3;
+            
+            Console.WriteLine("Nota final: " + NotaFim.ToString("f2", CultureInfo.InvariantCulture));
+            
+            if(NotaFim < 60.00)
+            {
+                Console.WriteLine("REPROVADO");
+                double diferenca = 60.00 - NotaFim;
+                Console.WriteLine("Faltam: " + diferenca.ToString("f2",CultureInfo.InvariantCulture) +" pontos");
+            }
+            else
+            {
+                Console.WriteLine("APROVADO");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioAlexGreen_Udemy004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno();
+
+            Console.Write("Digite o nome do Aluno: ");
+            aluno1.Nome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Difite as três notas do aluno");
+
+            aluno1.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            aluno1.SomaNota();
+        }
+    }
+}
